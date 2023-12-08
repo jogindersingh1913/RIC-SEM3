4a63354684596437516b913402509e3d9330647c
jogindersingh1913 <49472802+jogindersingh1913@users.noreply.github.com>
2023-12-08 05:52:10 +0000

    Update otherparaescapes.sh

--- otherparaescapes.sh
+++ otherparaescapes.sh
@@ -2,17 +2,23 @@
 
 check_container_isolation() {
     container_id=$1
-    echo $container_id
+    echo "Checking container: $container_id"
+    
+    # Initialize status to success
+    status="Success"
+
     # Scenario 1: Check if the container is running in privileged mode
     privileged=$(docker inspect --format '{{.HostConfig.Privileged}}' "$container_id")
     if [ "$privileged" == "true" ]; then
         echo "Container is running in privileged mode. Potential security risk."
+        status="Failure"
     fi
 
     # Scenario 2: Check for abnormal capabilities
     capabilities=$(docker inspect --format '{{.HostConfig.CapAdd}}' "$container_id")
     if [[ "$capabilities" == *"SYS_ADMIN"* ]]; then
         echo "Container has SYS_ADMIN capability. Potential security risk."
+        status="Failure"
     fi
 
     # Scenario 3: Check for mounted sensitive host directories
@@ -21,6 +27,7 @@ check_container_isolation() {
     for sensitive_mount in "${sensitive_mounts[@]}"; do
         if [[ "$mounts" == *"$sensitive_mount"* ]]; then
             echo "Container has mounted a sensitive host directory ($sensitive_mount). Potential security risk."
+            status="Failure"
         fi
     done
 
@@ -30,6 +37,7 @@ check_container_isolation() {
     for process in "${suspicious_processes[@]}"; do
         if echo "$container_processes" | grep -q "$process"; then
             echo "Container has a suspicious process ($process). Potential security risk."
+            status="Failure"
         fi
     done
 
@@ -38,17 +46,37 @@ check_container_isolation() {
     for attempt in "${breakout_attempts[@]}"; do
         if command -v "$attempt" > /dev/null; then
             echo "Common breakout tool '$attempt' is available on the host. Potential security risk."
+            status="Failure"
         fi
     done
 
-    # Additional checks can be added based on specific use cases and security policies
+    # Print the overall status
+    echo "Scenario status: $status"
 }
 
 # Replace 'your_container_id' with the actual container ID you want to inspect
 
 containerID=$(docker ps | awk '{print $1}' | grep -v CONTAINER)
 echo $containerID
+
+# Initialize overall status to success
+overall_status="Success"
+
 for cid in $containerID; do
-        echo "=================================================================================================================="
-        check_container_isolation "$cid"
+    echo "=================================================================================================================="
+    check_container_isolation "$cid"
+    
+    # Update overall status based on individual scenario status
+    if [ "$status" == "Failure" ]; then
+        overall_status="Failure"
+    fi
 done
+
+# Print overall status
+echo "Overall status: $overall_status"
+
+# Print a message if any scenario failed
+if [ "$overall_status" == "Failure" ]; then
+    echo "Hi, it failed!"
+    exit 1
+fi
