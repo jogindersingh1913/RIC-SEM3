5fc375053bc64411fde78ba272b01e100fca9f0d
jogindersingh1913 <49472802+jogindersingh1913@users.noreply.github.com>
2023-12-08 04:05:40 +0000

    Update DockerBuildCommands.sh

--- DockerBuildCommands.sh
+++ DockerBuildCommands.sh
@@ -2,4 +2,4 @@ sh 'whoami'
 // Build nginx image
 sh 'docker build -t nginx:latest -f nginx.Dockerfile .'
 // Build ubuntu image
-sh 'docker build -t ubuntu:latest -f ubuntu_escaped.Dockerfile .'
+//sh 'docker build -t ubuntu:latest -f ubuntu_escaped.Dockerfile .'
